dc3ca7f4ac260f7c847fd7f2bd22212b48864ef6
Majid Motallebikashani <chakmeshma@gmail.com>
2023-09-21 04:48:24 +0330

    Done (still not getting all results!)

--- display_seller_infos.py
+++ display_seller_infos.py
@@ -1,12 +1,12 @@
 import pickle
 
-sellers = dict()
 
 with open('sellerinfos', 'rb') as file_obj:
     sellers = pickle.load(file_obj)
 
 seller_keys = sellers.keys()
 
-for seller_key in seller_keys:
-    seller = sellers[seller_key]
-    print('{}\n\tEmail: {}\n\tPhone: {}'.format(seller['name'], seller['email'], seller['phone']))
+with open('sellers.txt', 'w') as file_obj:
+    for seller_key in seller_keys:
+        seller = sellers[seller_key]
+        file_obj.write('{}\n\tEmail: {}\n\tPhone: {}\n\n'.format(seller['name'], seller['email'], seller['phone']))
