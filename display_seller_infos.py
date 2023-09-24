e4d80241bc0589e7c91c6fed988723e5506891fd
Majid Motallebikashani <chakmeshma@gmail.com>
2023-09-24 14:11:31 +0330

    wide range all sellers

--- display_seller_infos.py
+++ display_seller_infos.py
@@ -1,12 +1,18 @@
 import pickle
 
-
 with open('sellerinfos', 'rb') as file_obj:
     sellers = pickle.load(file_obj)
 
 seller_keys = sellers.keys()
 
+err_count = 0
+
 with open('sellers.txt', 'w') as file_obj:
     for seller_key in seller_keys:
-        seller = sellers[seller_key]
-        file_obj.write('{}\n\tEmail: {}\n\tPhone: {}\n\n'.format(seller['name'], seller['email'], seller['phone']))
+        try:
+            seller = sellers[seller_key]
+            file_obj.write('{}\n\tEmail: {}\n\tPhone: {}\n\n'.format(seller['name'], seller['email'], seller['phone']))
+        except:
+            err_count += 1
+
+print(err_count)
